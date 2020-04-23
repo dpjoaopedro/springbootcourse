7e605a56d7b4e918d9888ab86909f6077878109b
Joao <dp.joaopedro@gmail.com>
2020-04-22 22:49:33 -0300

    added cors configuration

--- src/main/java/com/aplicacao/backend/resources/UserResource.java
+++ src/main/java/com/aplicacao/backend/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.aplicacao.backend.resources;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.aplicacao.backend.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping
 	public ResponseEntity<User> findAll() {
 		User u = new User(1L, "Maria", "maria@gmail.com", "9999", "12345");
