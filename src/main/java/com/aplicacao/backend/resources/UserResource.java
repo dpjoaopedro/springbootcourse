abba5bb1de7b2fac869da82e38e52c6a2678a66d
Joao <dp.joaopedro@gmail.com>
2020-04-23 09:06:24 -0300

    Service layer, component registration

--- src/main/java/com/aplicacao/backend/resources/UserResource.java
+++ src/main/java/com/aplicacao/backend/resources/UserResource.java
@@ -1,32 +1,36 @@
 package com.aplicacao.backend.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.aplicacao.backend.entities.User;
+import com.aplicacao.backend.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
-	@CrossOrigin(origins = "http://localhost:3000")
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		User u1 = new User(1L, "Maria", "maria@gmail.com", "9999", "12345");
-		User u2 = new User(2L, "Joao", "Joao@gmail.com", "9999", "12345");
-		User u3 = new User(3L, "Carlos", "carlos@gmail.com", "9999", "12345");
-		List<User> list = new ArrayList<>();
-		list.add(u1);
-		list.add(u2);
-		list.add(u3);
-
+		
+		List<User> list = service.findAll();
+		
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
