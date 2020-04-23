9cf57f4615cf78d42a2fd38e0fee6972b3af991b
Joao <dp.joaopedro@gmail.com>
2020-04-23 08:41:01 -0300

    H2 database, test profile JPA

--- src/main/java/com/aplicacao/backend/resources/UserResource.java
+++ src/main/java/com/aplicacao/backend/resources/UserResource.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.aplicacao.backend.entities.User;
 
 @RestController
@@ -14,9 +17,16 @@ public class UserResource {
 	
 	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		User u1 = new User(1L, "Maria", "maria@gmail.com", "9999", "12345");
+		User u2 = new User(2L, "Joao", "Joao@gmail.com", "9999", "12345");
+		User u3 = new User(3L, "Carlos", "carlos@gmail.com", "9999", "12345");
+		List<User> list = new ArrayList<>();
+		list.add(u1);
+		list.add(u2);
+		list.add(u3);
+
+		return ResponseEntity.ok().body(list);
 	}
 
 }
